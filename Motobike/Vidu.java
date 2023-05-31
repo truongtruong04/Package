c8d9e0f9f76e8fa39a5091b8408604601952d2cc
truongkhongsai <anhp31746@gmail.com>
2023-05-31 10:00:56 +0700

    fist commit

---
+++ Motobike/Vidu.java
@@ -0,0 +1,25 @@
+package Motobike;
+
+public class Vidu {
+    private int a , b;
+
+    public Vidu( int a , int b ){
+        super ();
+        this.a  = a ;
+        this.b = b ;
+    }
+    public int getA(){
+        return a;
+    }
+    public void  setA(int a ){
+        this.a = a ;
+
+    }
+    public int getB(){
+        return b ;
+
+    }
+    public void setB(int b ){
+        this.b = b ;
+    }
+}
---
+++ Test/Testvidu.java
@@ -0,0 +1,10 @@
+package Test;
+import Motobike.Vidu ;
+public class Testvidu {
+    public static void main(String [] args){
+        Vidu vd = new Vidu(7,8);
+        System.out.println(vd.getA());
+        System.out.println(vd.getB());
+
+    }
+}
